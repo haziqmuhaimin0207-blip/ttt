f5e0d7a08c2666fa09418feddee7961811f2425b
haziqmuhaimin0207-blip <haziqmuhaimin0207@gmail.com>
2026-08-09 18:52:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -110,6 +110,34 @@ function checkPassword() {
             passwordInput.classList.remove("shake");
         }, 500);
 
+    }/* Password error animation */
+
+.shake {
+    animation: shake 0.4s ease;
+}
+
+@keyframes shake {
+
+    0% {
+        transform: translateX(0);
     }
+
+    25% {
+        transform: translateX(-8px);
+    }
+
+    50% {
+        transform: translateX(8px);
+    }
+
+    75% {
+        transform: translateX(-8px);
+    }
+
+    100% {
+        transform: translateX(0);
+    }
+
+}
 
 }
